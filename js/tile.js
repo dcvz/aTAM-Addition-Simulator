ba30b09ba0a892a541ffe7a5c4fae6ee72a68a8a
fjjimenez23 <fjjimenez23@gmail.com>
2013-11-16 06:02:14 -0600

    Fixes on Website

--- js/tile.js
+++ js/tile.js
@@ -1,4 +1,39 @@
 
+var drawingCanvas = document.getElementById('Box');
+if(drawingCanvas.getContext) 
+{
+var context = drawingCanvas.getContext('2d');
+moveTo(0,0);
+}
+
+$("#input1").blur(function(){
+
+	var input1 = $("#input1").val();
+	console.log(input1);
+	for( i in input1)
+	{
+		if(input1[i]!='0' || input1[i]!='1')
+		{
+			$(".error").show();
+		}
+
+	}
+})
+
+$("#input2").blur(function(){
+
+	var input2 = $("#input2").val();
+	console.log(input2);
+	for( i in input2)
+	{
+		if(input2[i]!='0' || input2[i]!='1')
+		{
+			$(".error").show();
+		}
+
+	}
+})
+
 function getResults()
 {
 	$.ajax({url:"tileset.json",success:function(result){

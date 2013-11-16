bbb24d1fd4212107f44b24bcc853f0347dcd803f
jfoliva <j.oliva86@gmail.com>
2013-11-16 01:52:37 -0600

    stuff to parse tileset

--- js/tile.js
+++ js/tile.js
@@ -1,12 +1,34 @@
-Tile
+//Tile
 
 
+function getResults()
+{
+	$.ajax({crossDomain:true, url:"tileset.json"}).done(function(data)
+	{
+		var list = $.parseJSON(data);
+		console.log(list);
+	});
+};
+
+/*
+function glue( label, strength ) {
+	this.Label = label;
+	this.Strength = strength;
+}
+
+function tile( nglue, sglue, eglue, wglue ){
+	this.Nglue = nglue;
+	this.Sglue = sglue;
+	this.Eglue = eglue;
+	this.Wglue = wglue;
+}
+*/
+/*
 var Tile=new Object();
 Tile.Nglue= Nglue();
 Tile.Eglue= Eglue();
 Tile.Sglue= Sglue();
 Tile.Wglue= Wglue();
-
 
 var Nglue= new Object();
 Nglue.Label= ; 					//Nglue label input
@@ -23,3 +45,4 @@ Sglue.Strength= ;				//Sglue strength input
 var Wglue= new Object();
 Wglue.Label= ; 					//Wglue label input
 Wglue.Strength= ;				//Wglue strength input
+*/

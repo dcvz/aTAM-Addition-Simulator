fb706c8a532339a233a1d5da84b6e41d693b1434
jfoliva <j.oliva86@gmail.com>
2013-11-16 03:03:41 -0600

    deleted commented code

--- js/tile.js
+++ js/tile.js
@@ -1,5 +1,3 @@
-//Tile
-
 
 function getResults()
 {
@@ -14,40 +12,3 @@ function getResults()
     
   }});
 }
-
-/*
-function glue( label, strength ) {
-	this.Label = label;
-	this.Strength = strength;
-}
-
-function tile( nglue, sglue, eglue, wglue ){
-	this.Nglue = nglue;
-	this.Sglue = sglue;
-	this.Eglue = eglue;
-	this.Wglue = wglue;
-}
-*/
-/*
-var Tile=new Object();
-Tile.Nglue= Nglue();
-Tile.Eglue= Eglue();
-Tile.Sglue= Sglue();
-Tile.Wglue= Wglue();
-
-var Nglue= new Object();
-Nglue.Label= ; 					//Nglue label input
-Nglue.Strength= ;				//Nglue strength input
-
-var Eglue= new Object();
-Eglue.Label= ; 					//Eglue label input
-Eglue.Strength= ;				//Eglue strength input
-
-var Sglue= new Object();
-Sglue.Label= ; 					//Sglue label input
-Sglue.Strength= ;				//Sglue strength input
-
-var Wglue= new Object();
-Wglue.Label= ; 					//Wglue label input
-Wglue.Strength= ;				//Wglue strength input
-*/
